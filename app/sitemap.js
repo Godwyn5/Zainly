90972b7a9cc5aec5d03a9d8de6aaf37b6236b628
Godwyn5 <godwyn5@github.com>
2026-03-31 18:26:03 +0200

    feat: add sitemap.js for Google Search Console

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://zainly-alpha.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+  ]
+}
